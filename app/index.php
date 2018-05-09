ae99cb6a3005bac10aa9d668b0a5c65f53bc5b3f
lichunqiang <light-li@hotmail.com>
2018-05-10 00:09:22 +0800

    enhancement

--- app/index.php
+++ app/index.php
@@ -1,11 +1,22 @@
-<?php //phpinfo();
+<?php 
+//phpinfo();
+
+echo '<h1>MySQL</h1>';
 echo '<pre>';
 $handle = @mysqli_connect('mysql', 'docker', 'docker', 'develdb', 3306);
 echo mysqli_connect_error();
-        var_export($handle);
+var_export($handle);
+echo '</pre>';
 
 
+echo '<h1>Redis</h1>';
+echo '<pre>';
 $redis = new \Redis();
-        $redis->connect('redis');
-        var_export($redis->info());
+$redis->connect('redis');
+var_export($redis->info());
+echo '</pre>';
 
+echo '<h1>PHP Extensions</h1>';
+echo '<pre>';
+var_export(get_loaded_extensions());
+echo '</pre>';
